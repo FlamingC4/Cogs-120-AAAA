dcb282673210e860cb1119c666a73468120a5c54
Ateyib <aabdulka@ucsd.edu>
2018-02-26 11:34:31 -0800

    cancel works now

--- public/js/main.js
+++ public/js/main.js
@@ -10,16 +10,17 @@ var span = document.getElementsByClassName("exit")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+	console.log("exiting");
     modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
-    }
+    };
 }
