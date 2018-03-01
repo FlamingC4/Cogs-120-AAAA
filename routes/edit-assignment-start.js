b79c31155f525a9afa6d0863b43190b20be58abc
Anmol Arora <a6arora@ucsd.edu>
2018-02-28 16:51:33 -0800

    fixed routing issue

--- routes/edit-assignment-start.js
+++ routes/edit-assignment-start.js
@@ -2,12 +2,12 @@ var count = require("../cAssignment.json"); // I added this
 
 
 exports.view = function(req, res){
-  res.render('edit-assignment-from-start');
   count["viewAlt"] = false;
+  res.render('edit-assignment-from-start');
 };
 
 exports.viewAlt = function(req, res){
-  res.render('edit-assignment-from-start');
   count["viewAlt"] = true;
+  res.render('edit-assignment-from-start');
 };
 
