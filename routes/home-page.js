5808c424ba9d3f1744645c2e826b3fc33a779c8f
Aaron Wong <flamingc4@gmail.com>
2018-03-05 16:44:50 -0800

    cleaned up progress-checker

--- routes/home-page.js
+++ routes/home-page.js
@@ -1,10 +1,13 @@
 var assignmentList = require("../data.json");
+var curr = require("../cAssignment.json"); //added this
 
 exports.view = function(req, res){
   res.render('home-page', assignmentList);
+  //res.render('home-page', curr);
 };
 
 exports.viewAlt = function(req, res){
 	console.log("home page viewAlt");
   	res.render('home-page', assignmentList);
+  	//res.render('home-page', curr);
 };
