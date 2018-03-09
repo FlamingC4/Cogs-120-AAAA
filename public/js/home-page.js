e366d5b53a1f5d19404ec9519c024ed310d2be5b
Ateyib <aabdulka@ucsd.edu>
2018-03-08 23:32:05 -0800

    still working on moreInfo

--- public/js/home-page.js
+++ public/js/home-page.js
@@ -1,7 +1,36 @@
 'use strict';
+
+$(document).ready(function() {
+	initializePage();
+ });
+
+
+function initializePage() {
+	console.log("working");
+
+};
+
 var enterAssignmentName = new Audio('/enterAssignmentName.mp3');
+
+
 console.log(enterAssignmentName);
 $('#myBtn').onClick = function() {	
 	console.log("good");	
 	enterAssignmentName.play();	
 };
+
+var item = $(".thisAssignment");
+
+$(item).click(function(e) {
+	var myId = $(this).find(".prevAssignment").text();
+	var thisId = "#" + myId;
+	console.log(thisId);
+	var final = $(thisId);
+	$(final).toggleClass("hiddenInfo");
+	//var thisId = document.getElementById();
+	//console.log(thisId);
+	//$(thisId).toggleClass(".hiddenInfo");
+
+		//var name = $(this).text();
+		//console.log(name);
+});
--- public/js/loginValidate.js
+++ public/js/loginValidate.js
@@ -3,22 +3,19 @@ $(document).ready(function() {
  });
 
 
-
-
-
 function loginValidate(userName, password) {
 	var email = $("#email");
 	var password = $("#password");
 	var result = true;
 
 	if(email.val().length == 0) {
-		email.attr('placeholder', 'Please enter your email');
+		email.attr('placeholder', 'Please enter any email');
 		email.css('background', 'yellow');
 		email.addClass('placeholderred');
 		result = false;
 	}
 	if(password.val().length == 0) {
-		password.attr('placeholder', 'please enter your password');
+		password.attr('placeholder', 'Please enter any password');
 		password.css('background', 'yellow');
 		password.addClass('placeholderred');
 		result = false;
