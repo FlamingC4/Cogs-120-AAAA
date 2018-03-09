4a557f108c7cb0c014f92f601a5b0159bc82ff09
Ateyib <aabdulka@ucsd.edu>
2018-03-08 23:35:57 -0800

    done with more Info

--- public/js/home-page.js
+++ public/js/home-page.js
@@ -24,9 +24,10 @@ var item = $(".thisAssignment");
 $(item).click(function(e) {
 	var myId = $(this).find(".prevAssignment").text();
 	var thisId = "#" + myId;
+	var newId = document.getElementById(myId);
 	console.log(thisId);
 	var final = $(thisId);
-	$(final).toggleClass("hiddenInfo");
+	$(newId).toggleClass("hiddenInfo");
 	//var thisId = document.getElementById();
 	//console.log(thisId);
 	//$(thisId).toggleClass(".hiddenInfo");
