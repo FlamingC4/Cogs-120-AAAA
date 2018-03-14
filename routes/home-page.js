995035ad002a0db40b39b05b509de47d57f7acb3
Aaron Liu <a3liu@ucsd.edu>
2018-03-13 18:39:14 -0700

    Rerouted some stuff so that pause screen sends information

--- routes/home-page.js
+++ routes/home-page.js
@@ -6,6 +6,19 @@ exports.view = function(req, res){
   //res.render('home-page', curr);
 };
 
+exports.progressView = function(req, res) {
+  var restartInfo = req.body.resInfo;
+
+  var newInProgress = {
+    "name": curr.currAssignment.name,
+    "restart_date": restartInfo.resDate,
+    "restart_time": restartInfo.resTime,
+    "estimated_time" : curr.currAssignment.estimated_time
+  };
+
+  res.render('home-page', assignmentList);
+}
+
 exports.viewAlt = function(req, res){
 	console.log("home page viewAlt");
   	res.render('home-page', assignmentList);
