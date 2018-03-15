09272d4dfdb35d6058cc5102dc4eb81c5183342f
Anmol Arora <a6arora@ucsd.edu>
2018-03-14 18:12:58 -0700

    modal fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-var modal2 = document.getElementById("myModal2");
+//var modal2 = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtn2");
+//var btn2 = document.getElementById("myBtn2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("exit")[0];
-var span2 = document.getElementsByClassName("exit")[1];
+//var span2 = document.getElementsByClassName("exit")[1];
 
 var projectName = document.getElementById("project-name");
 
@@ -17,9 +17,9 @@ btn.onclick = function() {
     modal.style.display = "block";
 };
 
-btn2.onclick = function() {
+/*btn2.onclick = function() {
     modal2.style.display = "block";
-};
+}; */
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -27,10 +27,10 @@ span.onclick = function() {
     modal.style.display = "none";
 };
 
-span2.onclick = function() {
+/*span2.onclick = function() {
 	console.log("exiting");
     modal2.style.display = "none";
-};
+}; */
 
 function validate() {
 	if(projectName.value == 0) {
